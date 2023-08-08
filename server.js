263e4edea3c48c5372f0cef5d6b261fbb0490a2f
Patricio Mena <luispatricio.mh@gmail.com>
2023-08-08 16:48:58 -0600

    added middleware

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-app.use(cors());
-
 let db,
   dbConnectionString = process.env.DB_STRING,
   dbName = 'sample_mflix',
@@ -14,8 +12,18 @@ let db,
 MongoClient.connect(dbConnectionString).then((client) => {
   console.log('Connected to Database');
   db = client.db(dbName);
-  colletion = db.collection('movies');
+  collection = db.collection('movies');
 });
+
+app.set('view engine', 'ejs');
+// When someone visit our route page the public directory will give the file it needs
+app.use(express.static('public'));
+// Parse URLs
+app.use(express.urlencoded({ extended: true }));
+// Parse as json objects when sending and retrieving data
+app.use(express.json());
+//
+app.use(cors());
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
